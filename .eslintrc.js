9f901e0eff916ace7e5a7b114d4106de5f088224
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2025-02-09 22:15:44 +0100

    fix: linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   plugins: ['sort-destructure-keys'],
+  ignorePatterns: ['dist/', 'node_modules/', '.eslintrc.js'],
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
+    project: './tsconfig.json',
   },
   env: {
     es6: true,
