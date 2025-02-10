d6a7743751f63b8d819fdb99117b71ede3350d6a
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2025-02-10 23:14:09 +0100

    fix: ga

--- deploy.sh
+++ deploy.sh
@@ -2,28 +2,6 @@
 
 start=$(date +"%s")
 
-ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
-docker pull tericcabrel/node-rest-api:latest
-
-CONTAINER_NAME=bmiapp
-if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
-    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-        echo "Container is running -> stopping it..."
-        docker stop $CONTAINER_NAME;
-    fi
-fi
-
-docker run -d --rm -p 4800:4800 --name $CONTAINER_NAME tericcabrel/node-rest-api:latest
-
-exit
-ENDSSH
-
-if [ $? -eq 0 ]; then
-  exit 0
-else
-  exit 1
-fi
-
 end=$(date +"%s")
 
 diff=$(($end - $start))
